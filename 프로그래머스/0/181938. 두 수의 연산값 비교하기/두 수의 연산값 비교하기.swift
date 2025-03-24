2b9ea8b98aa1a203b35633f59cd4315a8b151690
Hamm1n <162417990+hyunm1n-o@users.noreply.github.com>
2025-03-24 10:24:11 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.05 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.swift
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.swift
@@ -1,6 +1,5 @@
 import Foundation
 
 func solution(_ a:Int, _ b:Int) -> Int {
-    let condition = Int("\(a)\(b)")! > (2*a*b)
-    return condition ? Int("\(a)\(b)")! : (2*a*b)
+    return max(Int("\(a)\(b)")!, 2*a*b)
 }
