a37a15872e20bf60fb44cffe583adfe0539da06e
Hamm1n <162417990+hyunm1n-o@users.noreply.github.com>
2025-03-24 13:15:21 +0900

    [level 0] Title: 조건 문자열, Time: 0.01 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.swift
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.swift
@@ -1,24 +1,11 @@
 import Foundation
 
 func solution(_ ineq:String, _ eq:String, _ n:Int, _ m:Int) -> Int {
-    var answer = 0
-    switch ineq {
-        case "<":
-            if eq == "=" {
-                answer = n <= m ? 1 : 0
-            }
-            else {
-                answer = n < m ? 1 : 0
-            }
-        case ">":
-         if eq == "=" {
-                answer = n >= m ? 1 : 0
-            }
-            else {
-                answer = n > m ? 1 : 0
-            }
-        default:
-        break
+    switch ineq+eq {
+        case "<=": return n <= m ? 1 : 0
+        case "<!": return n < m ? 1 : 0
+        case ">=": return n >= m ? 1 : 0
+        case ">!": return n > m ? 1 : 0
+        default: return 0
     }
-    return answer
 }
