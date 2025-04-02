44bd720b193dcf3aecf23a7d1e80ff34d17289b8
Hamm1n <162417990+hyunm1n-o@users.noreply.github.com>
2025-04-02 16:16:06 +0900

    [level 0] Title: 배열 만들기 4, Time: 52.52 ms, Memory: 20.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ arr:[Int]) -> [Int] {
+    var stk = [Int]()
+    var i = 0
+    
+    while i < arr.count {
+        if stk.isEmpty || stk.last! < arr[i] {
+            stk.append(arr[i])
+            i += 1
+        } else if stk.last! >= arr[i] {
+            stk.removeLast()
+        }
+    }
+    return stk
+}
